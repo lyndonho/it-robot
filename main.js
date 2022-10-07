bfd348b2aa3194d07a7b9d9c795db76740754aaf
chlin <lyndonho@users.noreply.github.com>
2022-10-07 09:38:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -40,71 +40,67 @@ async function main() {
   for (const count of counts) {
     console.log(await count.evaluate((el) => el.textContent));
   }
-
-  await browser.close();
   
-  const page = await browser.newPage();
-  await page.goto("https://ithelp.ithome.com.tw/users/20151567/ironman/5529?page=2");
+  const page2 = await browser.newPage();
+  await page2.goto("https://ithelp.ithome.com.tw/users/20151567/ironman/5529?page=2");
 
-  let index = 0;
+  let index2 = 0;
   while (true) {
-    const links = await page.$$(".qa-list .qa-list__title a");
+    const links2 = await page2.$$(".qa-list .qa-list__title a");
 
-    const link = links[index];
+    const link2 = links2[index2];
 
-    if (!link) break;
+    if (!link2) break;
 
     await Promise.all([
-      page.waitForNavigation(),
-      link.click(),
+      page2.waitForNavigation(),
+      link2.click(),
       //
     ]);
 
     await wait(600);
 
-    await page.goBack();
+    await page2.goBack();
 
-    index += 1;
+    index2 += 1;
   }
 
-  const counts = await page.$$(
+  const counts2 = await page2.$$(
     ".qa-list .qa-condition--change .qa-condition__count"
   );
-  for (const count of counts) {
-    console.log(await count.evaluate((el) => el.textContent));
+  for (const count2 of counts2) {
+    console.log(await count2.evaluate((el) => el.textContent));
   }
-
-  await browser.close();
   
-  const page = await browser.newPage();
-  await page.goto("https://ithelp.ithome.com.tw/users/20151567/ironman/5529?page=3");
+  const page3 = await browser.newPage();
+  await page3.goto("https://ithelp.ithome.com.tw/users/20151567/ironman/5529?page=3");
 
-  let index = 0;
+  let index3 = 0;
   while (true) {
-    const links = await page.$$(".qa-list .qa-list__title a");
+    const links3 = await page3.$$(".qa-list .qa-list__title a");
 
-    const link = links[index];
+    const link3 = links3[index3];
 
-    if (!link) break;
+    if (!link3) break;
 
     await Promise.all([
-      page.waitForNavigation(),
-      link.click(),
+      page3.waitForNavigation(),
+      link3.click(),
       //
     ]);
 
     await wait(600);
 
-    await page.goBack();
+    await page3.goBack();
 
-    index += 1;
+    index3 += 1;
   }
 
-  const counts = await page.$$(
+  const counts2 = await page2.$$(
     ".qa-list .qa-condition--change .qa-condition__count"
   );
-  for (const count of counts) {
-    console.log(await count.evaluate((el) => el.textContent));
+  for (const count2 of counts2) {
+    console.log(await count2.evaluate((el) => el.textContent));
   }
 
   await browser.close();
